5d8104ee79c631100395a8c6824d424121bea1ef
RANHAOHR <rxh349@case.edu>
2017-11-16 17:09:56 -0600

    edit policy functions

--- cartpole_gpu.py
+++ cartpole_gpu.py
@@ -1,10 +1,14 @@
 import tensorflow as tf
+#%matplotlib inline
 import math
 import numpy as np
-import cPickle as pickle
+try:
+    import cPickle as pickle
+except:
+    import pickle
 import gym
 import matplotlib.pyplot as plt
-%matplotlib inline
+
 
 env = gym.make('CartPole-v0') #load environment
 
@@ -21,40 +25,33 @@ state_size = 4
 action_size = 1
 hidden_size = 10
 
-init = tf.initialize_all_variables()
-
 def discount_rewards(r):
-    discount_rate = np.zero_like(r)
+    discount_rate = np.zeros_like(r)
     add_on = 0
     for t in reversed(range(0,r.size)):
         add_on = add_on* gamma + r[t]
         discount_rate[t] = add_on
     return discount_rate
 
-
-# self.state_in = tf.placeholder(shape = [None,state_size],dtype = tf.float32)
-# hidden = slim.fully_connected(self.state_in, hidden_size, biases_initializer = None, activation_fn = tf.nn.relu)
-# self.output = slim.fully_connected(hidden, action_size, activation_fn = tf.nn.softmax, biases_initializer = None)
-
 #now get the current reward and action to compute the policy gradient
-self.reward_holder = tf.placeholder(shape=[None],dtype=tf.float32)
-self.action_holder = tf.placeholder(shape=[None],dtype=tf.int32)
-self.observation = tf.placeholder(shape=[None, state_size], dtype=tf.float32)
+observations = tf.placeholder(tf.float32, [None, state_size], name="input_x")
 
+# ----------- structure of the network -----------
 W1 = tf.get_variable("W1", shape=[state_size, hidden_size], initializer=tf.contrib.layers.xavier_initializer())
 layer1 = tf.nn.relu(tf.matmul(observations,W1))
 W2 = tf.get_variable("W2", shape=[hidden_size, action_size],initializer=tf.contrib.layers.xavier_initializer())
 output = tf.matmul(layer1, W2)
 probability = tf.nn.sigmoid(output)
 
+# ----------- computing of the policy gradient -----------
 tvars = tf.trainable_variables()
 input_y = tf.placeholder(tf.float32,[None,1], name="output")
-advantages = tf.placeholder(tf.float32,name="discounted reward")
+advantages = tf.placeholder(tf.float32,name="reward_signal") # later computed at the end of each episode
 
-#input_y is 0 or 1 the likelihhod is probability or 1- probability
+#input_y is 0 or 1 the likelihood is probability or 1- probability
 log_likelihood = tf.log(input_y*(input_y - probability) + (1 - input_y)*(input_y + probability))
 
-loss = -tf.reduce_mean(loglik * advantages) 
+loss = -tf.reduce_mean(log_likelihood * advantages) 
 newGrads = tf.gradients(loss,tvars)
 
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
@@ -63,6 +60,7 @@ W2Grad = tf.placeholder(tf.float32,name="batch_grad2")
 batchGrad = [W1Grad,W2Grad]
 updateGrads = optimizer.apply_gradients(zip(batchGrad,tvars)) #apply after some episodes
 
+init = tf.global_variables_initializer()
 with tf.Session() as sess:
     rendering = False
     sess.run(init)
@@ -74,21 +72,22 @@ with tf.Session() as sess:
         gradBuffer[ix] = grad * 0
 
     while episode_number <= total_episodes:
+
         # Rendering the environment slows things down, 
-        # so let's only look at it once our agent is doing a good job.
+        # so let's only evaluate it once our agent is doing a good job.
         if reward_sum/batch_size > 100 or rendering == True : 
             env.render()
             rendering = True
     
         # Make sure the observation is in a shape the network can handle.
         x = np.reshape(observation,[1,state_size])
-        tfprob = sess.run(probability,feed_dict={observations: x})
         
         # Run the policy network and get an action to take. 
         tfprob = sess.run(probability,feed_dict={observations: x})
         action = 1 if np.random.uniform() < tfprob else 0 #if tfprob is bigger than 0.5, then action = 1
-        xs.append(x) # observation
-        y = 1 if action == 0 else 0 # a "fake label"
+
+        xs.append(x) # save observation
+        y = 1 if action == 0 else 0 #provide a mask to mark the action with the probabilities
         ys.append(y)
         
         # step the environment and get new measurements
@@ -112,6 +111,7 @@ with tf.Session() as sess:
 
             # Get the gradient for this episode, and save it in the gradBuffer
             tGrad = sess.run(newGrads,feed_dict={observations: epx, input_y: epy, advantages: discounted_epr})
+
             for ix,grad in enumerate(tGrad):
                 gradBuffer[ix] += grad
 
@@ -125,7 +125,7 @@ with tf.Session() as sess:
                 running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
                 print('Average reward for episode' + str(reward_sum/batch_size) + 'Total average reward' + str(running_reward/batch_size)) 
                 if reward_sum/batch_size > 200: 
-                    print "Task solved in",episode_number,'episodes!'
+                    print("Task solved in"+ str(episode_number)+'episodes!')
                     break
                 reward_sum = 0  
 
